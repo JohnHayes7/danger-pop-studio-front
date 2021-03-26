aa96a1b890fa8a1227e0de8d8c78735aa4df3c54
John Hayes <JohnnyHayes7@gmail.com>
2021-03-26 16:05:37 -0400

    added links to admin portal

--- src/components/AdminPortal.js
+++ src/components/AdminPortal.js
@@ -1,25 +1,37 @@
 import React from 'react'
 import './admin-style.css'
+import { Link } from 'react-router-dom'
+
 const AdminPortal = () =>{
+
+    
     return(
         <div>
             <h1>Administrator Portal</h1>
             <div className="admin-options">
                 <div className="options-rows">
-                    <div className="option">
-                        <div className="selectable-label">Users</div>
-                    </div>
-                    <div className="option">
-                        <div className="selectable-label">Appointments</div>
-                    </div>
+                    <Link to={'/admin/users'}>
+                        <div className="option">
+                            <div className="selectable-label"> Users</div>
+                        </div>
+                    </Link>
+                    <Link to={'/admin/appts'}>
+                        <div className="option">
+                            <div className="selectable-label">Appointments</div>
+                        </div>
+                    </Link>
                 </div>
                 <div className="options-rows">
-                    <div className="option">
-                        <div className="selectable-label">Projects</div>
-                    </div>
-                    <div className="option">
-                        <div className="selectable-label">Tattoo Requests</div>
-                    </div>
+                    <Link to={'/admin/projects'}>
+                        <div className="option">
+                            <div className="selectable-label">Projects</div>
+                        </div>
+                    </Link>
+                    <Link to={'/admin/tattoo-requests'}>
+                        <div className="option">
+                            <div className="selectable-label">Tattoo Requests</div>
+                        </div>
+                    </Link>
                 </div>
             </div>
         </div>
