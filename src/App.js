e30a6aaddfc0b43df706edca36401c61e89b563e
John Hayes <JohnnyHayes7@gmail.com>
2021-03-26 14:16:24 -0400

    added redux

--- src/App.js
+++ src/App.js
@@ -4,22 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="animated headline-2">Coming Soon</h1>
-      <img className='logo' src={"DangerPop_Logo_Black-BackGround.JPG"} alt="Danger Pop Logo" />
-      <div className="link-boxes">
-        <div className="box" id="left">
-          <a href={"https://www.instagram.com/danger_pop/?hl=en"}>
-            Tattoo Inquiries Click Here
-          </a>
-        </div>
-        
-          <div className="box" id="right">
-          <a href={"https://www.etsy.com/shop/DangerPopUpShop?ref=simple-shop-header-name&listing_id=880875255"}>
-              Clothing and Artwork Click Here
-          </a>
-          </div>
-        
-      </div>
+      <h1>Site Build #1</h1>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import thunk from 'redux-thunk'
+import { combineReducers } from 'redux'
 
 ReactDOM.render(
   <React.StrictMode>
