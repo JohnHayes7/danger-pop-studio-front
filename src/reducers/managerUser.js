a8797d6eb7205ce64b3d0eef4006c5fcea205bd1
John Hayes <JohnnyHayes7@gmail.com>
2021-03-29 15:57:21 -0400

    built out managerUser reducer

---
+++ src/reducers/managerUser.js
@@ -0,0 +1,32 @@
+export default function manageUser(state ={
+    loading: false,
+    all:[],
+    id: 0,
+    name: "",
+    email: "",
+    phonenumber:"",
+    address:"",
+    admin: false,
+    tattooapproved: false,
+    id_img_path:"",
+    allergies: "",
+    cart:{},
+    products:[],
+    projects:[],
+    tattoo_requests:[],
+    appointments:[],
+    loggedIn: false,
+}, action) {
+    switch(action.type){
+        
+        case 'LOADING_USER':
+            return {
+                ...state,
+                loading: true
+            }
+        
+        case 'ALL_USERS':
+            debugger
+            return null
+    }
+}
