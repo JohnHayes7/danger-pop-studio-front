a378b593ceaa2660a10da689e1eb8484d1371c5b
John Hayes <JohnnyHayes7@gmail.com>
2021-03-31 16:40:47 -0400

    added AdminTattooRequest component and debugged displaying details

---
+++ src/components/AdminTattooRequest.js
@@ -0,0 +1,29 @@
+import {React, useState} from 'react'
+import AdminTattooRequestDetails from './AdminTattooRequestDetails'
+
+const AdminTattooRequest = props => {
+    const [showReqDetails, setShowReqDetails] = useState(false)
+
+    const toggleShowReqDetails = () => setShowReqDetails(!showReqDetails)
+    
+    return (
+        <div  key={props.tr.id} data-id={props.tr.id} className="tattreq-attrs" onClick={toggleShowReqDetails} >
+            <div>
+                <div>Request ID: {props.tr.id}</div>
+                <div> {props.tr.attributes.user ? "Requestor has an account" : "Requested as a guest"}</div>
+                <div>Requestor's Email: {props.tr.attributes.user ? props.tr.attributes.user.email : props.tr.attributes.guest_email}</div>
+                {props.tr.attributes.user ? <div>Client Name: {props.tr.attributes.user.name}</div> : null}
+                {props.tr.attributes.user ? <div>Previously Tattoo Approved? {props.tr.attributes.user.tattoo_approved ? "Yes" : "No"}</div> : null}
+                <div>Description: {props.tr.attributes.description}</div>
+                <br></br>
+                <AdminTattooRequestDetails tr={props.tr} showReqDetails={showReqDetails}/>
+            </div>
+            <br></br>
+            <div><strong>{props.tr.attributes.approved ? "This Request has been approved" : "This Request has not yet been approved"}</strong></div>
+            <br></br>
+            <button>Approve?</button>
+        </div>
+    )
+}
+
+export default AdminTattooRequest
--- src/components/AdminTattooRequests.js
+++ src/components/AdminTattooRequests.js
@@ -1,10 +1,11 @@
 import {React, useState, useEffect} from 'react'
 import './admintattoorequests.css'
 import AdminTattooRequestDetails from './AdminTattooRequestDetails'
+import AdminTattooRequest from './AdminTattooRequest'
 
 const AdminTattooRequests = () => {
     const [requests, setRequests] = useState([])
-    const [showReqDetails, setShowReqDetails] = useState(false)
+    
     const [selectedRequest, setSelectedRequest] = useState([])
    
     useEffect(() => {
@@ -15,56 +16,10 @@ const AdminTattooRequests = () => {
         })
     }, [] )
 
-    // const select = (e, tr) => {
-    //     e.preventDefault()
-        
-    // }
 
-    const toggleShowReqDetails = (e, tr) => {
-        debugger
-        if(e.currentTarget.dataset.id === tr.id){
-            return setShowReqDetails(!showReqDetails)
-        }else{
-            return showReqDetails
-        }
-    }
+    const parseAllTattooRequests = () => requests.data ? requests.data.map( tr => <AdminTattooRequest tr={tr}/>) : null
         
         
-        
-    
-
-
-    const parseAllTattooRequests = () =>{
-        
-        return requests.data ? requests.data.map( tr => <div  key={tr.id} data-id={tr.id} className="tattreq-attrs" onClick={(e) => toggleShowReqDetails(e, tr)} >
-                <div>
-                    <div>Request ID: {tr.id}</div>
-                    <div> {tr.attributes.user ? "Requestor has an account" : "Requested as a guest"}</div>
-                    <div>Requestor's Email: {tr.attributes.user ? tr.attributes.user.email : tr.attributes.guest_email}</div>
-                    {tr.attributes.user ? <div>Client Name: {tr.attributes.user.name}</div> : null}
-                    {tr.attributes.user ? <div>Previously Tattoo Approved? {tr.attributes.user.tattoo_approved ? "Yes" : "No"}</div> : null}
-                    <div>Description: {tr.attributes.description}</div>
-                    < AdminTattooRequestDetails tr={tr} showReqDetails={showReqDetails}/>
-                </div>
-                <div><strong>{tr.attributes.approved ? "This Request has been approved" : "This Request has not yet been approved"}</strong></div>
-                
-                <button>Approve?</button>
-        </div>) : null
-        
-    }
-
-    const requestDetails = (tr) =>{
-        if(showReqDetails){
-            debugger
-            return(
-                <div >
-                    <h1>Request Details</h1>
-                    {tr.id}
-                </div>
-            )
-        }
-    }
-
     return(
         <div>
             <h1>Administrator Tattoo Requests Portal</h1>
