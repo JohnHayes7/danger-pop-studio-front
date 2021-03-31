d21e6045be6d6010acad95884a3fa79d58243fd8
John Hayes <JohnnyHayes7@gmail.com>
2021-03-31 13:52:45 -0400

    debugging request details display

--- src/components/AdminTattooRequests.js
+++ src/components/AdminTattooRequests.js
@@ -14,37 +14,41 @@ const AdminTattooRequests = () => {
         })
     }, [], showReqDetails)
 
-    const select = e => {
+    const select = (e, tr) => {
         e.preventDefault()
+        toggleShowReqDetails()
         debugger
     }
 
-    const toggleShowReqDetails = (e) =>{
+    const toggleShowReqDetails = () => setShowReqDetails(!showReqDetails)
         
-       return (
-           setShowReqDetails(!showReqDetails)
-       )
-    }
+       
 
 
     const parseAllTattooRequests = () =>{
         
-        return requests.data ? requests.data.map( tr => <div  key={tr.id}className="tattreq-attrs" onClick={e => select(e)}>
-            <div>Request ID: {tr.id}</div>
-            <div> {tr.attributes.user ? "Requestor has an account" : "Requested as a guest"}</div>
-            <div>Requestor's Email: {tr.attributes.user ? tr.attributes.user.email : tr.attributes.guest_email}</div>
-            {tr.attributes.user ? <div>Client Name: {tr.attributes.user.name}</div> : null}
-            {tr.attributes.user ? <div>Previously Tattoo Approved? {tr.attributes.user.tattoo_approved ? "Yes" : "No"}</div> : null}
-            <div>Description: {tr.attributes.description}</div>
-            <div><strong>{tr.attributes.approved ? "This Request has been approved" : "This Request has not yet been approved"}</strong></div>
+        return requests.data ? requests.data.map( tr => <div  key={tr.id}className="tattreq-attrs" onClick={e => select(e, tr)} >
+                
+                <div>Request ID: {tr.id}</div>
+                <div> {tr.attributes.user ? "Requestor has an account" : "Requested as a guest"}</div>
+                <div>Requestor's Email: {tr.attributes.user ? tr.attributes.user.email : tr.attributes.guest_email}</div>
+                {tr.attributes.user ? <div>Client Name: {tr.attributes.user.name}</div> : null}
+                {tr.attributes.user ? <div>Previously Tattoo Approved? {tr.attributes.user.tattoo_approved ? "Yes" : "No"}</div> : null}
+                <div>Description: {tr.attributes.description}</div>
+                <div>{requestDetails(tr)}</div>
+                <div><strong>{tr.attributes.approved ? "This Request has been approved" : "This Request has not yet been approved"}</strong></div>
+               
         </div>) : null
+        
     }
 
-    const requestDetails = () =>{
+    const requestDetails = (tr) =>{
         if(showReqDetails){
+            debugger
             return(
                 <div id="tattreq-details">
                     <h1>Request Details</h1>
+                    {tr.id}
                 </div>
             )
         }
@@ -55,7 +59,7 @@ const AdminTattooRequests = () => {
             <h1>Administrator Tattoo Requests Portal</h1>
             <div className="tattoo-request-panel">
                 <div>{parseAllTattooRequests()}</div>
-                <div>{requestDetails()}</div>
+                
             </div>
             
         </div>
