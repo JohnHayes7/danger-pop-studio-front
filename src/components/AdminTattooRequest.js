d1e78327c0f89be191e6d0108d5c5173fabbd897
John Hayes <JohnnyHayes7@gmail.com>
2021-04-01 16:39:40 -0400

    toggle between large and small images

--- src/components/AdminTattooRequest.js
+++ src/components/AdminTattooRequest.js
@@ -10,8 +10,8 @@ const AdminTattooRequest = props => {
    
     return (
 
-        <div  key={props.tr.id} data-id={props.tr.id} className="tattreq-attrs" onClick={toggleShowReqDetails} >
-            <div>
+        <div  key={props.tr.id} data-id={props.tr.id} className="tattreq-attrs"  >
+            <div onClick={toggleShowReqDetails}>
                 <div>Request ID: {props.tr.id}</div>
                 <div> {props.tr.attributes.user ? "Requestor has an account" : "Requested as a guest"}</div>
                 <div>Requestor's Email: {props.tr.attributes.user ? props.tr.attributes.user.email : props.tr.attributes.guest_email}</div>
@@ -19,8 +19,8 @@ const AdminTattooRequest = props => {
                 {props.tr.attributes.user ? <div>Previously Tattoo Approved? {props.tr.attributes.user.tattoo_approved ? "Yes" : "No"}</div> : null}
                 <div>Description: {props.tr.attributes.description}</div>
                 <br></br>
-                <AdminTattooRequestDetails tr={props.tr} showReqDetails={showReqDetails}/>
             </div>
+                <AdminTattooRequestDetails tr={props.tr} showReqDetails={showReqDetails}/>
             <br></br>
             <div><strong>{props.tr.attributes.approved ? "This Request has been approved" : "This Request has not yet been approved"}</strong></div>
             <br></br>
--- src/components/AdminTattooRequestDetails.js
+++ src/components/AdminTattooRequestDetails.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import {React, useState} from 'react'
 
 
 const AdminTattooRequestDetails = (props) => {
-    debugger
+    const [imageWidth, setImageWidth] = useState("220px")
+    const [imageHeight, setImageHeight] = useState("250px")
+    
 
     const parseNotes = () => props.tr.attributes.notes.map( n => <li key={n.id}>{n.content}</li>)
+
+    const fullScreen = () =>{
+        setImageWidth("75vw")
+        setImageHeight("90vh")
+    }
+
+    const fitToDetail = () =>{
+        setImageWidth("220px")
+        setImageHeight("250px")
+    }
+
+    const toggleFullScreen = () => imageWidth === "220px" ? fullScreen() : fitToDetail()    
     
-    debugger
     return(
         <div id="tattreq-details" style={{display: props.showReqDetails ? 'block' : 'none'}}>
             <div>Body Location Image: <br></br>
-                <img className="body-location-image" src={props.tr.attributes.body_location_image_path} alt='location image of requested tattoo'/>
+                <img onClick={toggleFullScreen}className="body-location-image" src={props.tr.attributes.body_location_image_path} style={{height: imageHeight, width: imageWidth}}alt='location image of requested tattoo'/>
             </div>
             <br></br>
             <div>{props.tr.attributes.notes.length > 0 ? <ul> Notes: {parseNotes()} </ul> : "No Notes for this request"}</div>
