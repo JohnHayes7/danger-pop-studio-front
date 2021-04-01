ddee0a53fdb90f3d5ebf423acfdcfef068ad1c18
John Hayes <JohnnyHayes7@gmail.com>
2021-04-01 15:18:09 -0400

    debugging adding images

--- src/components/AdminTattooRequestDetails.js
+++ src/components/AdminTattooRequestDetails.js
@@ -4,12 +4,12 @@ import React from 'react'
 const AdminTattooRequestDetails = (props) => {
     debugger
 
-    const parseNotes = () => props.tr.attributes.notes.map( n => <li>{n.content}</li>)
+    const parseNotes = () => props.tr.attributes.notes.map( n => <li key={n.id}>{n.content}</li>)
     
 
     return(
         <div id="tattreq-details" style={{display: props.showReqDetails ? 'block' : 'none'}}>
-            <div>Body Location Image:{props.tr.attributes.body_location_image_path}</div>
+            <div>Body Location Image:<img src={'arms_prioritize_scr1.jpeg'} alt='location image of requested tattoo'/></div>
             <br></br>
             <div>{props.tr.attributes.notes.length > 0 ? <ul> Notes: {parseNotes()} </ul> : "No Notes for this request"}</div>
         </div>
