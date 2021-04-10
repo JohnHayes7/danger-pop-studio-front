ceceaf310c25da23a26b3b65c5f4de16dc21631f
John Hayes <JohnnyHayes7@gmail.com>
2021-04-10 00:04:31 -0400

    working on project display

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,7 +1,10 @@
 import {React, useState, useEffect} from 'react'
+import Navbar from '../Nav/Navbar'
+import './projectcss.css'
 
 const Project = (props) => {
-    const [state, setState] = useState([])
+    const [project, setProject] = useState({})
+    
     // const [pageId, setPageId] = useState("")
 
     const pageId = props.location.pathname.split('/').splice(-1)
@@ -10,16 +13,53 @@ const Project = (props) => {
         // setPageId()
         fetch(`http://localhost:3001/projects/${pageId}`).then(response => response.json())
         .then(rxData => {
-            // console.log(rxData)
-            setState(rxData)
-            console.log(state)
+           
+            setProject(rxData.data)
         })
-    }, [])
+    }, {})
+
+    
+
+    const projectModules = () =>{
+        debugger
+        return(
+            <div >
+                <h1>{project.attributes ? projectModule()  : null }</h1>
+            </div>
+        )
+     
+    }
+
+    const projectModule = () => {
+        return(
+            <div className="project-modules" >
+                <div>
+                    <div className='proj-mod'>{project.attributes.user.name}</div>
+                    <div className='proj-mod'>{project.attributes.user.name}</div>
+                </div>
+                <div>
+                    <div className='proj-mod'>{project.attributes.user.name}</div>
+               </div>
+                
+            </div>
+            
+            
+        )
+    }
+
+   
 
     return(
         <div>
-            <h1>Danger Pop Administrator Project Page</h1>
+            <Navbar />
+            <div className='project-data-display'>
+                <h1>Danger Pop Project Page</h1>
+                <div >
+                    {projectModules()}
+                </div>
+            </div>
         </div>
+        
     )
 }
 
