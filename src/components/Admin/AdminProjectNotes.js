3b08ea37a6772a4ecc636c430f828821c2d2b9a0
John Hayes <JohnnyHayes7@gmail.com>
2021-04-16 14:19:29 -0400

    deleteing project note capability

--- src/components/Admin/AdminProjectNotes.js
+++ src/components/Admin/AdminProjectNotes.js
@@ -1,6 +1,7 @@
 import {React, useState} from 'react'
 import AdminProjectNoteForm from './AdminProjectNoteForm'
 import './adminprojectnoteformcss.css'
+import axios from 'axios'
 
 const AdminProjectNotes = (props) =>{
 
@@ -8,15 +9,31 @@ const AdminProjectNotes = (props) =>{
 
     const toggleProjectNotesForm = () => setShowProjNoteForm(!showProjNoteForm)
 
+    const deleteNote = (e) =>{
+        e.preventDefault()
+        // debugger
+        deleteNoteFromDb(e.currentTarget.dataset.id)
+    }
+
+    const deleteNoteFromDb = (id) =>{
+        axios({method: 'DELETE', url: `http://localhost:3001/project_notes/${id}`,   headers: {'Content-Type': 'application/json'}}).then(resp => {
+            console.log(resp)
+            // refresh()
+          }).catch( err => {  
+            //catch the error
+            console.log(err)
+          })
+    }
+
    
     const parseNotes = () =>{
        debugger
         if(props.project.attributes.project_notes.length > 0){
             return (
                 <div>
-                    <li>
-                        {props.project.attributes.project_notes.map(n => <li>{n.content}</li> )} 
-                    </li>
+                    <ul>
+                        {props.project.attributes.project_notes.map(n => <li key={n.id}><div>{n.content}<div data-id={n.id}className="edit-note-button" onClick={e => deleteNote(e)}>X</div></div></li> )} 
+                    </ul>
                 </div>
             )
         }else{
