c495161b3a636653962250db0163ead5875120cb
John Hayes <JohnnyHayes7@gmail.com>
2021-06-16 15:56:16 -0400

    building out admin form

--- src/components/Fullscreen/FullscreenAdminForm.js
+++ src/components/Fullscreen/FullscreenAdminForm.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Field from '../InputFields/Field'
+import axios from 'axios'
 
 const FsAdminForm = () =>{
+
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const emailInput = (e) => {
+        debugger
+        setEmail(e.target.value)
+        // setEmail()
+    }
+
+    const passInput = (e) =>{
+        setPassword(e.target.value)
+    }
+
+
+    const saveNewAdmin = (e) =>{
+        
+    }
+
     return(
         <div className='admin-option'>
             <h3>Add New Administrator</h3>
             <form> 
                 <div className='admin-input-fields'>
-                    <Field label='Administrator Email' id="email"/>
-                    <Field label='password' id='password' />
+                    <Field label='Administrator Email' id="email" changeHandler={e => emailInput(e)}/>
+                    <Field label='password' id='password' changeHandler={e => passInput(e)}/>
                 </div>
-                <div className='submit-button'>
+                <div className='submit-button' onClick={e => saveNewAdmin(e)}>
                     Create New Admin
                 </div>
             </form>
