f565b1e589a9205fa818106a59e96e619e2ec838
John Hayes <JohnnyHayes7@gmail.com>
2021-06-17 15:46:01 -0400

    added fullscreen images to project tattoo request module

--- src/components/Admin/AdminProjectTattooRequest.js
+++ src/components/Admin/AdminProjectTattooRequest.js
@@ -2,11 +2,15 @@ import {React, useState} from 'react'
 import S3FileUpload from 'react-s3'
 import axios from 'axios'
 import Refresh from '../Utilites/Refresh'
+import FullScreen from '../Fullscreen/Fullscreen'
 import GetCurrentUser from '../Utilites/CurrentUser'
 
 const AdminProjectTattooRequest = (props) =>{
     const [selectedImage, setSelectedImage] = useState(false)
     const [showUpload, setShowUpload] = useState(false)
+    const [showBodyPreview, setShowBodyPreview] = useState(true)
+    const [showMockupPreview, setShowMockupPreview] = useState(true)
+
     const config = {
         bucketName: process.env.REACT_APP_S3_BUCKET_NAME,
         dirName: process.env.REACT_APP_S3_PROJECT_MOCKUP_IMAGES,
@@ -50,24 +54,30 @@ const AdminProjectTattooRequest = (props) =>{
           })  
         }
 
-        const displayImageOrUploadOption = () =>{
-           debugger
-           if(!!props.project.attributes.tattoo_request.mockupImageLocation){
-               return <img className="image-preview" src={props.project.attributes.tattoo_request.mockupImageLocation} alt="Mockup Tattoo Image" />
-           }else{
-               return props.user.attributes.administrator === true ? <input id="progress-images" className="input" type="file" name="file" onChange={e => fileChange(e)}/> : null
-           }
-          
+    const displayImageOrUploadOption = () =>{
+        debugger
+        if(!!props.project.attributes.tattoo_request.mockupImageLocation){
+            return <img className="image-preview" onClick={toggleMockupPreview} src={props.project.attributes.tattoo_request.mockupImageLocation} alt="Mockup Tattoo Image" />
+        }else{
+            return props.user.attributes.administrator === true ? <input id="progress-images" className="input" type="file" name="file" onChange={e => fileChange(e)}/> : null
         }
+        
+    }
+
+    const toggleBodyPreview = () => setShowBodyPreview(!showBodyPreview)
+    const toggleMockupPreview = () => setShowMockupPreview(!showMockupPreview)
+
     return(
         <div>
             <div>Request ID: {props.project.attributes.tattoo_request.id}</div>
             <div> Location on Body:</div>
             <div>
-               <img className={props.imageDisplayClass()} onClick={props.togglePreviewImage} src={props.project.attributes.tattoo_request.body_location_image_path} alt="body image location" />
+               <img className={props.imageDisplayClass()} onClick={toggleBodyPreview} src={props.project.attributes.tattoo_request.body_location_image_path} alt="body image location" />
+                {!showBodyPreview ? <FullScreen type="image" toggle={toggleBodyPreview} imageSource={props.project.attributes.tattoo_request.body_location_image_path}/> : null}
             </div>
             <div> Mockup Image:</div>
             {/* <input id="progress-images" className="input" type="file" name="file" onChange={e => fileChange(e)}/><br></br> */}
+                {!showMockupPreview ? <FullScreen type="image" toggle={toggleBodyPreview} imageSource={props.project.attributes.tattoo_request.mockupImageLocation}/> : null}
                 {displayImageOrUploadOption()}
             <div> 
                {showUpload  ? <button onClick={uploadMockup}>Upload Mockup Image</button> : null}
