00a6747a82f1cf3a945b8ea6f8c16c183f8df090
John Hayes <JohnnyHayes7@gmail.com>
2021-06-23 17:22:45 -0400

    adding fullscreen request feature

--- src/components/Fullscreen/FulllscreenRequest.js
+++ src/components/Fullscreen/FulllscreenRequest.js
@@ -11,6 +11,14 @@ const FullscreenRequest = (props) => {
                     <div>Email: {props.project.attributes.guest_email}</div>
                     <div>Description: {props.project.attributes.description}</div>
                 </div>
+                <div className='fs-top-options'>
+                    <h1>Request Options:</h1><br></br>
+                    <span className='request-options'>Accept {"&"} Schedule</span><span className='request-options'>Accept {"&"} Schedule Later</span><span className='request-options'>Decline</span>
+                </div>
+            </div>
+            <div class='fs-bottom'>
+                <h1>Body Location Image:</h1>
+                <img className="fs-body-location-image" src={props.project.attributes.body_location_image_path} alt='body-location' />
             </div>
         </div>
     )
