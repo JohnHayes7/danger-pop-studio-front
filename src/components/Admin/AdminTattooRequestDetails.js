33206e91db69a2ac09ccaf02fc775102fcc0cb04
John Hayes <JohnnyHayes7@gmail.com>
2021-09-28 14:06:59 -0400

    added x out functionality to fullscreen

--- src/components/Admin/AdminTattooRequestDetails.js
+++ src/components/Admin/AdminTattooRequestDetails.js
@@ -26,7 +26,7 @@ const AdminTattooRequestDetails = (props) => {
     
     return(
         <div id="tattreq-details" style={{display: props.showReqDetails ? 'block' : 'none'}}>
-             {showFullScreen ? <FullScreen type="image" imageSource={props.tr.attributes.body_location_image_path}/> : null}
+             {showFullScreen ? <FullScreen toggle={toggleFullScreen} type="image" imageSource={props.tr.attributes.body_location_image_path}/> : null}
             <div>Allergies: {props.tr.attributes.allergies}</div>
             <div>Body Location Image: <br></br>
                 <img onClick={toggleFullScreen}className="body-location-image" src={props.tr.attributes.body_location_image_path} style={{height: imageHeight, width: imageWidth}}alt='location image of requested tattoo'/>
--- src/components/Fullscreen/FullscreenImage.js
+++ src/components/Fullscreen/FullscreenImage.js
@@ -3,10 +3,9 @@ import React from 'react'
 const FullscreenImage = (props) =>{
 
     return( 
-        <div className="full-screen" onClick={props.toggle}>
+        <div className="full-screen">
+            <div><span className='exit-x' onClick={props.toggle}>X</span> </div>
             <img className="fs-image" src={props.imageSource} alt='body image' />
-            
-
         </div>
     )
 }
